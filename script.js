0c78e9e7107ba6dafcddb9e41f460a9cbb9228f6
edwintantawi <edwintantawi.workspace@gmail.com>
2023-02-27 10:04:59 +0700

    feat: add html, css, js files

---
+++ script.js
@@ -0,0 +1,119 @@
+const articles = [
+  {
+    title:
+      'Life hardships pave my way to getting IDR 140 million incubation funds.',
+    summary:
+      'A Story of Nuryadin Abutani, Bangkit 2022 Graduate from Informatics Engineering,',
+    image: {
+      source:
+        'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi1sEFHAc6Yo3gJt67ZKlwIL00EPIdAlFbUW1FfdsUu8l5U3Y0gPPCMO8zhqbObUsGSRQmibgEbbpdR1J6og7s5QQ1dRiKvXQMN-r_dFMd975umMKDr1OwBCx-UGvRJSXQMaCfbXmeXflDG16tjObdteUDN6xg1x6JscaUTe5ROp4jnMgQ0aSSgasXEzg/s1015/%5BBLOG%20BANNER%5D%20Nuryadin%20Abutani%20-%20Bangkit%202022%20Alumni.jpg',
+      title: 'Nuryadin Abutani',
+    },
+    url: 'https://blog.bangkit.academy/2022/09/life-hardships-pave-my-way-to-getting.html',
+    date: '2022-09-29',
+  },
+  {
+    title: 'Learning from Scratch then Earning TFD Certification.',
+    summary:
+      'A Story of Riza Isna Khoirun Nisa, Bangkit 2022 Student from Mathematics,',
+    image: {
+      source:
+        'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEh_mr9P5InNhVji02vMlO1tgAGJZd7gdTD3_tqcim_IDeJOeOXvP8xfTaa4gRqghufeLOY4hInOtFhu5O_TdI1N6PlZdt3it3Gsisi22D3orx1bJ2k50BRcIVv4IlQgvQNeGITJo4bYaR6RutyJ0Ulu_IIds3QIgFAB406z2pofDzKofMKHuc62WJULKQ/s1015/%5BBLOG%20BANNER%5D%20Riza%20Isna%20Khoirun%20Nisa%20-%20Bangkit%202022%20Alumni.jpg',
+      title: 'Riza Isna Khoirun Nisa',
+    },
+    url: 'https://blog.bangkit.academy/2022/09/learning-from-scratch-then-earning-tfd.html',
+    date: '2022-09-23',
+  },
+  {
+    title: 'Relying on Internet Cafe(s) to Give the Best in Bangkit.',
+    summary:
+      'A Story of Agung Izzul Haq, Bangkit 2022 Graduate from Electrical Engineering,',
+    image: {
+      source:
+        'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjEfr1HQ1gbJ2hqePTANT4Ugr9LZRF-sPvDDi-A5IWq1god6gCVQqq2940cjKhswU5sP5RWNjuwOfZ04ggmRlZeIP8jDIOxLWREWFB2DKzYPftQK6gkjdlD8f0w_1s1lyk2O1--_8RGUlUthOkDzr1PyAsjzUed9q4rtWflAdjzbBqeq0TGp8RYp0ahvg/s1015/%5BBLOG%20BANNER%5D%20Agung%20Izzul%20Haq%20-%20Bangkit%202022%20Alumni.jpg',
+      title: 'Agung Izzul Haq',
+    },
+    url: 'https://blog.bangkit.academy/2022/09/relying-on-internet-cafes-to-give-best.html',
+    date: '2022-09-20',
+  },
+  {
+    title: 'Great achievements take time.',
+    summary:
+      'A Story of Nauval Muhammad Firdaus, Bangkit 2022 Graduate from Information Technlogy,',
+    image: {
+      source:
+        'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgbvFXmwzrRyWNZh8-DEgBRriQmVmisPtk0MTcGm-8zPB7qzQZuzOdg4P3zGBWg29aC4wWEP5LC7yxVKlMliwDGhcUKBx6AjA-T0_6hoDdekkLcrE_gI6ypAY9yIbmq-V1akMr3Gyw4U6ql0Oz04A1cLAuX1796GIFXcIj6_rrFprXQhtkqGINpYf94sg/s1015/%5BBLOG%20BANNER%5D%20Nauval%20Muhammad%20Firdaus%20-%20Bangkit%202022%20Alumni.jpg',
+      title: 'Nauval Muhammad Firdaus',
+    },
+    url: 'https://blog.bangkit.academy/2022/09/great-achievements-take-time.html',
+    date: '2022-09-15',
+  },
+  {
+    title: 'Bangkit: When Tech and Soft Skills Come in One Package.',
+    summary:
+      'A Story of Fachirah Hestiana Rachmawati, Bangkit 2021 Graduate from Computer Science,',
+    image: {
+      source:
+        'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEij4pzZIzZSngS3FebMDOJPXhC39OwPSRZSJTLFPjb5SGG8-wXMj9eewE-R-liPKqEICopjP_fWqUuFx-XouSUyMWIFNf9ISLZaoukpYcs9CERpOG9OwOWVwuTEWgz56cyrquuodNUD7fgJEohN8vsCBGkVC8BPIt8RkJ7Z78Ano61ZQupPmw-lsVXlhQ/s1015/%5BBLOG%20BANNER%5D%20Fachirah%20Hestiana%20Rachmawati%20-%20Bangkit%202021%20Alumni.jpg',
+      title: 'Fachirah Hestiana Rachmawati',
+    },
+    url: 'https://blog.bangkit.academy/2022/09/bangkit-when-tech-and-soft-skills-come.html',
+    date: '2022-09-12',
+  },
+  {
+    title: 'Remember that you are always bigger than your problems.',
+    summary:
+      'A Story of Amar Luthfi, Bangkit 2022 Student from Informatics Engineering, Universitas',
+    image: {
+      source:
+        'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiCiUeS-VwlVnbmaurWacUNTcTf9teEt_umyWqZGClK2ZxPHLnYRY1yL3N7HfrvJG1-WtBhjgbx9OxcZeZGZRWHBfVYVbtph3ZxO64ZPX7JCjhXEKrFUGYXfgBFaAh-zNCTVAzXNatFClLmDlLgi2Ep_xSd2I0GZeSHouuVYB6wSBx33OsqLDsu_wK2og/s1015/%5BBLOG%20BANNER%5D%20Amar%20Luthfi%20-%20Bangkit%202022%20Cohort.jpg',
+      title: 'Amar Luthfi',
+    },
+    url: 'https://blog.bangkit.academy/2022/09/remember-that-you-are-always-bigger.html',
+    date: '2022-09-08',
+  },
+];
+
+function formatDate(date) {
+  return new Intl.DateTimeFormat('id-ID', {
+    dateStyle: 'long',
+    timeZone: 'Asia/Jakarta',
+  }).format(date);
+}
+
+function createArticle(article) {
+  return `
+    <article class="article">
+        <header>
+            <img
+                loading="lazy"
+                src="${article.image.source}"
+                alt="${article.image.title}"
+                class="article__image"
+            />
+            <time datetime="${article.date}" class="article__date">
+                ${formatDate(new Date(article.date))}
+            </time>
+            <h2 class="article__title">
+                <a href="${article.url}" target="_blank" class="article__link">
+                    ${article.title}
+                </a>
+            </h2>
+        </header>
+        <p class="article__summary">
+            ${article.summary}
+        </p>
+    </article>
+    `;
+}
+
+function renderArticles(articles) {
+  const articleContainer = document.getElementById('article-container');
+  const articleTemplates = [];
+  for (const article of articles) {
+    articleTemplates.push(createArticle(article));
+  }
+  articleContainer.innerHTML = articleTemplates.join('');
+}
+
+renderArticles(articles);
